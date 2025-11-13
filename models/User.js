5c8feaa9b6fa78c47f908b1c46cb922ebfacbd24
Devansh Ganatra <140813128+DevanshGanatra@users.noreply.github.com>
2025-11-14 02:04:35 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,16 +4,16 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  passwordHash: String,
+  password: String,
   role: { type: String, enum: ['admin','waiter'], default: 'waiter' }
 }, { timestamps: true });
 
 UserSchema.methods.setPassword = async function(password) {
-  this.passwordHash = await bcrypt.hash(password, 10);
+  this.password = await bcrypt.hash(password, 10);
 };
 
 UserSchema.methods.comparePassword = async function(password) {
-  return bcrypt.compare(password, this.passwordHash);
+  return bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('User', UserSchema);
